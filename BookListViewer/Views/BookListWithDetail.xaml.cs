e90debd1af12d614b42a656e65cf5872a51de43b
drminor <david.minor@live.com>
2018-06-13 21:09:23 -0400

    Renamed event handler: Button_click to btnClose_Click.

--- BookListViewer/Views/BookListWithDetail.xaml.cs
+++ BookListViewer/Views/BookListWithDetail.xaml.cs
@@ -1,16 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace BookListViewer.Views
 {
@@ -24,7 +12,7 @@ namespace BookListViewer.Views
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void btnClose_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
         }
