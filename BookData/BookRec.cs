99bc3393c0c7ea6f4bb8d060bb1f614234a7c689
drminor <david.minor@live.com>
2018-06-13 22:10:02 -0400

    Deleted the class: Class1.

--- BookData/BookRec.cs
+++ BookData/BookRec.cs
@@ -1,16 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BookData
 {
+    //public class BookTitlePlusId
+    //{
+
+    //}
+
     public class BookRec
     {
+        public int Id { get; set; }
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public BookGenre BookGenre { get; set; }
+        public Decimal Price { get; set; }
+        public DateTime PublishDate { get; set; }
+        public string Description { get; set; }
+    }
 
-
-
-
+    public enum BookGenre
+    {
+        Computer,
+        Fantasy,
+        Romance,
+        Horror
     }
 }
