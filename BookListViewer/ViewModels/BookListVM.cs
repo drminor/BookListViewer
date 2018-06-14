106587ba3f38f55afad2d589ac24a920a12674a7
drminor <david.minor@live.com>
2018-06-14 08:04:07 -0400

    Added TODO: comment regarding fetching the book records asynchronously.

---
+++ BookListViewer/ViewModels/BookListVM.cs
@@ -0,0 +1,46 @@
+﻿using BookData;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace BookListViewer.ViewModels
+{
+    public class BookListVM : INotifyPropertyChanged
+    {
+        private ObservableCollection<BookRec> _books = new ObservableCollection<BookRec>();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public BookListVM(List<BookRec> lst)
+        {
+            ObservableCollection<BookRec> temp = new ObservableCollection<BookRec>();
+
+            foreach(BookRec br in lst)
+            {
+                temp.Add(br);
+            }
+
+            Books = temp;
+        }
+
+        public ObservableCollection<BookRec> Books
+        {
+            get
+            {
+                return _books;
+            }
+
+            set
+            {
+                _books = value;
+                OnPropertyChanged(nameof(Books));
+            }
+        }
+
+        private void OnPropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+
+    }
+}
--- BookListViewer/Views/BookListWithDetail.xaml.cs
+++ BookListViewer/Views/BookListWithDetail.xaml.cs
@@ -1,7 +1,8 @@
-﻿using System.Windows;
+﻿using BookData;
 using BookDataReaderXML;
 using System.Collections.Generic;
-using BookData;
+using System.Windows;
+using BookListViewer.ViewModels;
 
 namespace BookListViewer.Views
 {
@@ -15,7 +16,13 @@ namespace BookListViewer.Views
         public BookListWithDetail()
         {
             Reader br = new Reader();
-            List<BookRec> lst = br.FetchBookData(XML_DATA_PATH);
+
+            // TODO: Create two new methods: BeginFetchBookData and EndFetchBookData
+            // and have the calling window create the Reader and call the BeginFetchBookData method.
+            // This constuctor can then call EndFetchBookData, having been passed a reference to the Reader.
+            List<BookRec> catalog = br.FetchBookData(XML_DATA_PATH);
+
+            this.DataContext = new BookListVM(catalog);
 
             InitializeComponent();
         }
